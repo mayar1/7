7b2c7ec142f9c620e18776f7679a2aafa54be5d2
mayar1 <HH@MaytekiiMac.local>
2015-12-13 14:33:05 -0800

    Xcode for slim7 groupwork

--- 7/ViewController.swift
+++ 7/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var WebView: UIWebView!
+    
+    
+    var URLPath = "http://maryanndul.com/squishy89x/AAU/Fall_2015/WebTech/slim7/final/index.php"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+         loadAddressURL()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func loadAddressURL() {
+        let requestURL = NSURL (string: URLPath)
+        let request = NSURLRequest(URL: requestURL!)
+        WebView.loadRequest(request)
 
 }
 
+}
